1cc05f2b9e864c3d99a8711abb26fc56a086dbe4
Thanh Nguyen <thanh@symphonycreativesolutions.com>
2016-02-24 22:00:12 +0700

    Initial Commit

---
+++ IOSFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSFramework
+//
+//  Created by Thanh Nguyen on 2/24/16.
+//  Copyright © 2016 Thanh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
